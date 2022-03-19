2638794340ae93b4a7ecdbb236af69f609cfe334
wangbo <pmb2020@163.com>
2022-03-19 21:08:20 +0800

    '轮播图'

--- app/Http/Controllers/Api/BannerController.php
+++ app/Http/Controllers/Api/BannerController.php
@@ -11,6 +11,24 @@ class BannerController extends Controller
     public function index(){
         $url = 'api/goods/topic/carouse-list';
         $res = DataokeService::getClient()->request($url,[],'v2.0.0');
+        foreach ($res as &$v){
+            if ($v['sourceType'] == 2){
+                $v['click_url']=$this->test($v['activityId'])['click_url'];
+            }
+        }
         return apiSuccess($res);
+    }
+
+    /**
+     * 获取淘宝活动会场链接
+     * @param $activityId
+     */
+    public function test($activityId){
+        $url = 'api/tb-service/activity-link';
+        $params = [
+            'promotionSceneId' =>$activityId
+        ];
+        $res = DataokeService::getClient()->request($url,$params,'v1.0.0');
+        return $res;
     }
 }
